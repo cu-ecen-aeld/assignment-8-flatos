af74799a1b7cecc7dedb59ad027437c38095ce2a
Frank Latos <flatos@duosys.com>
2024-12-25 10:40:35 -0500

    A8 Try 4

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,8 +6,8 @@
 #
 ##############################################################
 
-# LDD_VERSION = 8d8d577		#'#COMMIT VERSION NUMBER'
-LDD_VERSION = 9999999		#'#COMMIT VERSION NUMBER'
+# AESDCHAR_VERSION = 8d8d577		#'#COMMIT VERSION NUMBER'
+AESDCHAR_VERSION = 9999999		#'#COMMIT VERSION NUMBER'
 
 
 
@@ -16,16 +16,19 @@ LDD_VERSION = 9999999		#'#COMMIT VERSION NUMBER'
 # Your site should start with git@github.com
 
 #'#GITHUB REPOSITORY LINK'
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-flatos.git
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-flatos.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
 #LDD_MODULE_SUBDIRS = scull misc-modules		#
-LDD_MODULE_SUBDIRS += aesd-char-driver
+
+AESDCHAR_MODULE_SUBDIRS += aesd-char-driver
+# AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
 # see https://buildroot.org/downloads/manual/manual.html#_infrastructure_for_packages_building_kernel_modules
-LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+AESDCHAR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 
-define LDD_INSTALL_TARGET_CMDS
+define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
 
